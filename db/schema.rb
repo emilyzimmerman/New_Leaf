7303defcf72642444d49b280ddded5e4efa34544
emilyzimmerman <zimmermanemily121@gmail.com>
2023-02-26 17:55:12 -0600

    ADDED Reviews
    Added relation between review to activities and users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_02_24_012733) do
+ActiveRecord::Schema[7.0].define(version: 2023_02_26_231831) do
   create_table "activities", force: :cascade do |t|
     t.string "name"
     t.string "description"
@@ -41,6 +41,17 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_24_012733) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "reviews", force: :cascade do |t|
+    t.string "content"
+    t.datetime "date"
+    t.integer "activity_id", null: false
+    t.integer "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["activity_id"], name: "index_reviews_on_activity_id"
+    t.index ["user_id"], name: "index_reviews_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "username"
     t.string "email"
@@ -50,4 +61,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_24_012733) do
     t.string "password_digest"
   end
 
+  add_foreign_key "reviews", "activities"
+  add_foreign_key "reviews", "users"
 end
